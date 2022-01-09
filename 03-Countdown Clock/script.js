9ff328d01f922d952dbe39ae99ae1d29c53a0354
Sriniketh J <81156510+srini047@users.noreply.github.com>
2022-01-09 18:35:52 +0530

    updated proj-3 js file

--- 03-Countdown Clock/script.js
+++ 03-Countdown Clock/script.js
@@ -5,7 +5,4 @@ var countdownVal = countdownElement.innerHTML;
 var interval = setInterval(function () {
   countdownVal = countdownVal > 0 ? countdownVal - 1 : 0;
   countdownElement.innerHTML = countdownVal;
-  if (countdownVal <= 3) {
-      
-  }
 }, 1000);
